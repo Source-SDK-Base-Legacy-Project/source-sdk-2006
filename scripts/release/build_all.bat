af9dc124ccf2b93dcf79395de954bb55ea11bfa8
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2024-07-20 10:52:53 -0400

    Add build scripts.

---
+++ scripts/release/build_all.bat
@@ -0,0 +1,14 @@
+setlocal enableextensions enabledelayedexpansion
+prompt $
+echo off
+cls
+
+set _version=1.0.0
+set _platform_RID=win-x86
+
+call build.bat "..\.." %_version% %_platform_RID% game\episodic source_2006_mod_episodic
+call build.bat "..\.." %_version% %_platform_RID% game\hl2 source_2006_mod_hl2
+call build.bat "..\.." %_version% %_platform_RID% game\hl2mp source_2006_mod_hl2mp
+call build.bat "..\.." %_version% %_platform_RID% game\sdksample source_2006_mod_sdksample
+
+endlocal
---
+++ scripts/sourcemods/publish.bat
@@ -0,0 +1,17 @@
+setlocal enableextensions enabledelayedexpansion
+prompt $
+@echo off
+cls
+
+rem https://superuser.com/questions/1566985/read-value-from-registry-via-batch-file-without-showing-the-source
+
+for /f "tokens=2*" %%a in ('REG QUERY "HKEY_CURRENT_USER\SOFTWARE\Valve\Steam" /v "SourceModInstallPath"') do set "_sourcemodinstallpath=%%~b"
+
+set _root=..\..
+
+call ..\base\copy_dir.bat "%_root%\game\episodic" "%_sourcemodinstallpath%\source_2006_mod_episodic"
+call ..\base\copy_dir.bat "%_root%\game\hl2" "%_sourcemodinstallpath%\source_2006_mod_hl2"
+call ..\base\copy_dir.bat "%_root%\game\hl2mp" "%_sourcemodinstallpath%\source_2006_mod_hl2mp"
+call ..\base\copy_dir.bat "%_root%\game\sdksample" "%_sourcemodinstallpath%\source_2006_mod_sdksample"
+
+endlocal
